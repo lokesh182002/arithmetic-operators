e27f348c067089a4fa5d2f518bb229f3d3035a28
Lokesh pemmanaboina <84330548+lokesh182002@users.noreply.github.com>
2021-08-22 21:57:51 +0530

    Add files via upload

---
+++ arithematic operators.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,add,sub,mult,rem;
+	float div;
+	a=5;
+	b=2;
+	add=a+b;
+	sub=a-b;
+	mult=a*b;
+	div=a/b;
+	rem=a%b;
+	printf("add=%d\n",add);
+	printf("sub=%d\n",sub);
+	printf("mult=%d\n",mult);
+	printf("div=%fs\n",div);
+		
+}
